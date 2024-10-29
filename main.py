9e8f5d66c758aed4234c502b3d2492bc45576e4f
jakub-rudnik <jakub.rudnik@interia.pl>
2024-10-29 02:25:05 +0100

    Improved model (CNN, Dropout)

--- main.py
+++ main.py
@@ -11,29 +11,40 @@ from keras import layers
 train_images = train_images/255
 test_images = test_images/255
 
-# print(train_images.shape)
-# print(train_labels.shape)
-#
-# plt.imshow(train_images[0], cmap='Greys')
-# plt.show()
-
 model = tf.keras.models.Sequential([
-        layers.Input(train_images.shape[1:]),
-        layers.Flatten(),
-        layers.Dense(10, activation='softmax')
-    ])
+    # layers.Input(train_images.shape[1:]),
+    # layers.Flatten(),
+    #~104k
+    # layers.Dense(128, activation='relu'),
+    # layers.Dropout(0.3),
+    # layers.Dense(10, activation='softmax')
+    #acc: ~0.97
 
-# one-hot representation
-# model.compile(optimizer='adam', loss='categorical_crossentropy')
+    layers.Input(train_images.shape[1:]),
+    # Dodawanie kanału koloru do obrazu (1 - skala szarości)
+    layers.Reshape((28, 28, 1)),
+    # 32 filtrów, każdy o wymiarach 3x3 z funkcją aktywacji relu
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    # wybranie najważniejszych wartości w każdym obszarze 2x2
+    layers.MaxPooling2D((2, 2)),
+    # Druga warstwa konwolucyjna
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    # Spłaszczenie danych do wektora (Z 2 WYMIARÓW NA JEDEN)
+    layers.Flatten(),
+    #225,034
+    layers.Dense(128, activation='relu'),
+    #wyłaczanie 30% neuronów w warstwie aby uniknąć overfittingu (nadmiernego dopasowania)
+    layers.Dropout(0.3),
+    #10 neuronów wyjściowych (odpowiadających cyfrom 0-9) z funkcją aktywacji softmax
+    layers.Dense(10, activation='softmax')
+    #acc: ~0.99
+])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# model.summary()
+model.summary()
 
-# One-hot encoding and training
-# labels_onehot_train = tf.one_hot(train_labels, 10)
-# model.fit(train_images, labels_onehot_train)
-
-model_history = model.fit(train_images, train_labels, epochs=10, batch_size=128, validation_data=(test_images, test_labels), verbose=False)
+model_history = model.fit(train_images, train_labels, epochs=30, batch_size=64, validation_data=(test_images, test_labels), verbose=False)
 
 # wykresy historii uczenia
 plt.plot(model_history.history['loss'], label='train')
@@ -52,11 +63,13 @@ plt.show()
 # testowanie modelu
 model.evaluate(test_images, test_labels)
 
-probs = model.predict(test_images[:5])
-preds = np.argmax(probs, axis=1)
+probs = model.predict(test_images[64:67])
+predictions = np.argmax(probs, axis=1)
 
 for i in range(3):
-    print(probs[i], "=>", preds[i])
+    print(probs[i], "=>", predictions[i])
+
+for i in range(64,67):
     plt.imshow(test_images[i], cmap='Greys')
     plt.show()
 
